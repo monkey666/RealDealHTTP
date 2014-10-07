255576607ef3cba856faa730865b0136a3b74b87
Kwoth <srki0509@gmail.com>
2014-10-07 23:57:19 +0200

    testing and playing around

--- RealDealHTTP/SimpleWebServer.cs
+++ RealDealHTTP/SimpleWebServer.cs
@@ -22,7 +22,7 @@ class SimpleWebServer
     public void run()
     {
         server.Listen(10);
-        while (true)
+        for ( ; ; )
         {
             Socket socket = server.Accept();
             HandleClientSession(socket);
@@ -83,8 +83,10 @@ class SimpleWebServer
     }
     private void HandleClientSession(Socket socket)
     {
-        Console.WriteLine("**New Request**");
         String first = SocketReadLine(socket);
+        if (first == null || first == "")
+            return;
+        Console.WriteLine("**New Request**");
         Console.WriteLine(first);
 
         // read in headers and post data
@@ -92,19 +94,112 @@ class SimpleWebServer
         Console.WriteLine(headerString);
 
         // write the HTTP response
-        SocketWrite(socket, "HTTP/1.1 200 OK");
-        SocketWrite(socket, "");
-        SocketWrite(socket, "<html>");
-        SocketWrite(socket, "<head><title>Simple Web Server</title></head>");
-        SocketWrite(socket, "<body>");
-        SocketWrite(socket, "<h1> KONICHIWA SEKAI </h1>");
-        SocketWrite(socket, "</body>");
-        SocketWrite(socket, "</html>");
+        char[] delim = { ' ' };
+        String[] tok = first.Split(delim);
+        String verb = tok[0];
+        String path = tok[1];
+        String version = tok[2];
 
+        if (String.Compare(verb, "GET", true) == 0)
+            SendFile(socket, path);
+        else
+            Error(socket, 500, "Unsupported command");
         // close everything up
         socket.Close();
     }
+    private void Transmit(Socket socket, int code, String message, byte[] body, String contentType)
+    {
+        StringBuilder headers = new StringBuilder();
+        headers.Append("HTTP/1.1 ");
+        headers.Append(code);
+        headers.Append(' ');
+        headers.Append(message);
+        headers.Append("\n");
+        headers.Append("Content-Length: " + body.Length + "\n");
+        headers.Append("Server: Heaton Research Example Server\n");
+        headers.Append("Connection: close\n");
+        headers.Append("Content-Type: " + contentType + "\n");
+        headers.Append("\n");
+        socket.Send(encoding.GetBytes(headers.ToString()));
+        socket.Send(body);
+    }
+    private void Error(Socket socket, int code, String message)
+    {
+        StringBuilder body = new StringBuilder();
+        body.Append("<html><head><title>");
+        body.Append(code + ":" + message);
+        body.Append("</title></head><body><p>An error occurred.</p><h1>");
+        body.Append(code);
+        body.Append("</h1><p>");
+        body.Append(message);
+        body.Append("</p></body></html>");
+        Transmit(socket, code, message, encoding.GetBytes(body.ToString()), "text/html");
+    }
+    private void AddSlash(StringBuilder path)
+    {
+        if (!path.ToString().EndsWith("\\"))
+            path.Append("\\");
+    }
+    private String GetContent(String path)
+    {
+        path = path.ToLower();
+        if (path.EndsWith(".jpg") || path.EndsWith(".jpeg"))
+            return "image/jpeg";
+        else if (path.EndsWith(".gif"))
+            return "image/gif";
+        else if (path.EndsWith(".png"))
+            return "image/png";
+        else
+            return "text/html";
+    }
+    private void SendFile(Socket socket, String path)
+    {
+        char[] delim = { '/' };
 
+
+        // parse the file by /'s and build a local file
+        String[] tok = path.Split(delim);
+        Console.WriteLine(path);
+        StringBuilder physicalPath = new StringBuilder(httproot);
+        AddSlash(physicalPath);
+
+        foreach (String e in tok)
+        {
+            if (!e.Trim().Equals("\\"))
+            {
+                if (e.Equals("..") || e.Equals("."))
+                {
+                    Error(socket, 500, "Invalid request");
+                    return;
+                }
+                AddSlash(physicalPath);
+                physicalPath.Append(e);
+            }
+        }
+
+        if (physicalPath.ToString().EndsWith("\\"))
+        {
+            physicalPath.Append("index.html");
+        }
+
+        String filename = physicalPath.ToString();
+        // open the file and send it if it exists
+        FileInfo file = new FileInfo(filename);
+        if (file.Exists)
+        {
+            // send the file
+            FileStream fis = File.Open(filename, FileMode.Open);
+            byte[] buffer = new byte[(int)file.Length];
+            fis.Read(buffer, 0, buffer.Length);
+            fis.Close();
+            this.Transmit(socket, 200, "OK", buffer, GetContent(filename));
+        }
+        // file does not exist, so send file not found
+        else
+        {
+            this.Error(socket, 404, "File Not Found");
+        }
+    }
     [STAThread]
     static void Main(string[] args)
     {
@@ -115,7 +210,6 @@ class SimpleWebServer
             try
             {
                 //main stuff
-
                 SimpleWebServer webServer = new SimpleWebServer(Int32.Parse(args[0]), args[1]);
                 webServer.run();
 
---
+++ UploadValuesPost/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        WebClient wc = new WebClient();
+
+        NameValueCollection data = new NameValueCollection(2);
+        data.Add("user", "kwoth");
+        data.Add("passwd", "lalal");
+        byte[] response;
+        response = wc.UploadValues("http://reddit.com", "POST", data);
+        Console.WriteLine("done");
+
+        Program.SaveToFile(Encoding.ASCII.GetString(response));
+        Console.ReadKey();
+    }
+    public static void SaveToFile(string text){
+       // if (File.Exists(@"D:\response.html"))
+      File.Delete(@"D:\response.html");
+      File.WriteAllText(@"D:\response.html", text);
+    }
+}
